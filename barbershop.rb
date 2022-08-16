cf59a04108aefe31e0cdc2250ef3be4558cfebfd
Artem134134 <car322132@gmail.com>
2022-08-16 23:22:01 +0300

    added hairdresser selection

--- barbershop.rb
+++ barbershop.rb
@@ -7,23 +7,25 @@ get '/' do
 end
 
 post '/' do
+
+	@hairdresser = params[:hairdresser]
 	@user_name = params[:user_name]
 	@phone = params[:phone]
 	@date_time = params[:date_time]
 
 	@title = 'Thank you!'
-	@message = "Dear visitor: #{@user_name} 
+	@message = "Dear visitor: #{@user_name},
+	 your hairdresser: #{@hairdresser}, 
 	we'll be waiting for you at #{@date_time}."
 
 	
 	f = File.open("./public/users.txt", "a")
-	f.write "User: #{@user_name}
+	f.write "Hairdresser: #{@hairdresser}
+	  User: #{@user_name}
 	  Phone: #{@phone}
 	  Date and time: #{@date_time}\n"
 	f.close
-	  erb :message
-
-	
+	  erb :message	
 end
 
 get '/admin' do
